594bb3f8e87133306f194d017a8c63fdf99305b0
youeo <yaj5414@gmail.com>
2026-06-18 18:02:04 +0900

    환경변수 전달 테스트13

--- terraform-AWS/modules/workload/outputs.tf
+++ terraform-AWS/modules/workload/outputs.tf
@@ -2,6 +2,10 @@ output "cluster_name" {
   value = aws_eks_cluster.main.name
 }
 
+output "cluster_id" {
+  value = aws_eks_cluster.main.id
+}
+
 output "cluster_endpoint" {
   value = aws_eks_cluster.main.endpoint
 }
